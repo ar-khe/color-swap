f11ac27f3b2cf12a7c2203de618442901343a8bb
ar-khe <jvegapichardo@gmail.com>
2025-05-29 13:26:54 -0500

    added ability to read palettes from file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-use image::{ImageReader};
-use std::{env, path::PathBuf, process::exit};
+use image::ImageReader;
+use std::{env, fmt::Error, path::PathBuf, process::exit};
 
 mod palette;
 
-fn main() {
+fn main() -> Result<(), Error> {
+    let palette_path = PathBuf::from(env::args().nth(1).expect(
+        "No path given\nExecute program as 'cargo run -- path/to/palette.txt path/to/file.png'",
+    ));
+    let palette = palette::parse_palette(&palette_path).expect("Palette couldn't be parsed");
 
-    let path = PathBuf::from(env::args().nth(1).expect("No path given\nExecute program as 'cargo run -- path/to/file.png'"));
-    println!("Loading Image {:?}", path.file_name().expect("Image name couldn't be parsed"));
-    let image_reader = ImageReader::open(&path).expect("Image couldn't be opened");
+    let image_path = PathBuf::from(env::args().nth(2).expect("No image path given"));
+    println!(
+        "Loading Image {:?}",
+        image_path
+            .file_name()
+            .expect("Image name couldn't be parsed")
+    );
+    let image_reader = ImageReader::open(&image_path).expect("Image couldn't be opened");
     let mut image = image_reader.decode().expect("Image couldn't be decoded");
-    let palette = palette::gruvbox_palette();
+    // let palette = palette::gruvbox_palette();
 
     println!("Modifying Image...");
     match palette::change_image_palette(&palette, image) {
@@ -19,23 +28,29 @@ fn main() {
             exit(1);
         }
     }
-    
+
     println!("Saving Image...");
     // save image
-    let parent_path = &path
+    let parent_path = &image_path
         .parent()
         .expect("Parent path couldn't be resolved")
         .to_str()
         .expect("Parent path couldn't be cast to string");
-    let file_name = &path
+    let file_name = &image_path
         .file_name()
         .expect("File name couldn't be resolved")
         .to_str()
         .expect("Filename couldn't be cast to string");
+    let palette_name = palette_path
+        .file_stem()
+        .expect("Couldn't parse palette name")        
+        .to_str()
+        .expect("Palette name couldn't be cast to string");
 
-    let new_path = format!("{}/{}_{}", parent_path, palette.name, file_name);
+    let new_path = format!("{}/{}_{}", parent_path, palette_name, file_name);
 
     image.save(&new_path).expect("Couldn't save image");
     println!("Image saved as \"{}\"", new_path);
-}
 
+    Ok(())
+}
--- src/palette.rs
+++ src/palette.rs
@@ -1,22 +1,37 @@
-use std::{sync::Mutex};
+use anyhow::{bail, Result};
 use image::{DynamicImage, GenericImage, GenericImageView, Rgb, Rgba};
 use indicatif::ProgressBar;
 use rayon::iter::{ParallelBridge, ParallelIterator};
+use std::{
+    fs::File, io::{self, BufRead}, iter::zip, path::Path, sync::Mutex
+};
+use thiserror::Error;
+
+
+#[derive(Debug, Clone)]
 pub struct Palette {
-    pub name: &'static str,
-    pub colors: Vec<Rgb<u8>>
+    pub colors: Vec<Rgb<u8>>,
+}
+
+#[derive (Debug, Error)]
+enum PaletteErrors{
+    #[error("")]
+    ParseRGBError,
+
+    #[error("")]
+    NotEnoughDataError,
 }
 
 impl Palette {
-    pub fn closest_to<'a>(&'a self, color: &Rgba<u8>)->Rgba<u8>{
+    pub fn closest_to<'a>(&'a self, color: &Rgba<u8>) -> Rgba<u8> {
         closest_color(color, &self.colors)
     }
 }
 
-fn closest_color<'a>(color: &Rgba<u8>, colors: &'a Vec<Rgb<u8>>)->Rgba<u8>{
+fn closest_color<'a>(color: &Rgba<u8>, colors: &'a Vec<Rgb<u8>>) -> Rgba<u8> {
     let mut closest: &Rgb<u8> = colors.get(0).unwrap();
     let mut closest_distance: f64 = distance(color, closest);
-    for c in colors{
+    for c in colors {
         let current_distance: f64 = distance(color, c);
         if current_distance < closest_distance {
             closest = &c;
@@ -27,80 +42,93 @@ fn closest_color<'a>(color: &Rgba<u8>, colors: &'a Vec<Rgb<u8>>)->Rgba<u8>{
     return Rgba([closest[0], closest[1], closest[2], color[3]]);
 }
 
-fn distance(c1: &Rgba<u8>, c2: &Rgb<u8>)->f64{
-    let delta_r = if c2.0[0] > c1.0[0] {((c2.0[0] - c1.0[0]) as u64).pow(2)} else {((c1.0[0] - c2.0[0]) as u64).pow(2)};
-    let delta_g = if c2.0[1] > c1.0[1] {((c2.0[1] - c1.0[1]) as u64).pow(2)} else {((c1.0[1] - c2.0[1]) as u64).pow(2)};
-    let delta_b = if c2.0[2] > c1.0[2] {((c2.0[2] - c1.0[2]) as u64).pow(2)} else {((c1.0[2] - c2.0[2]) as u64).pow(2)};
+fn distance(c1: &Rgba<u8>, c2: &Rgb<u8>) -> f64 {
+    let delta_r = if c2.0[0] > c1.0[0] {
+        ((c2.0[0] - c1.0[0]) as u64).pow(2)
+    } else {
+        ((c1.0[0] - c2.0[0]) as u64).pow(2)
+    };
+    let delta_g = if c2.0[1] > c1.0[1] {
+        ((c2.0[1] - c1.0[1]) as u64).pow(2)
+    } else {
+        ((c1.0[1] - c2.0[1]) as u64).pow(2)
+    };
+    let delta_b = if c2.0[2] > c1.0[2] {
+        ((c2.0[2] - c1.0[2]) as u64).pow(2)
+    } else {
+        ((c1.0[2] - c2.0[2]) as u64).pow(2)
+    };
 
-    f64::sqrt((delta_r+delta_g+delta_b) as f64)
+    f64::sqrt((delta_r + delta_g + delta_b) as f64)
 }
 
-pub fn change_image_palette(palette: &Palette, image: DynamicImage)->Result<DynamicImage, std::sync::PoisonError<DynamicImage>>{
+pub fn change_image_palette(palette: &Palette, image: DynamicImage) -> anyhow::Result<DynamicImage> {
     let clone = image.clone();
     let (width, heigth) = image.dimensions();
     let image_mutex = Mutex::new(image);
-    let progress_bar = ProgressBar::new({width*heigth} as u64);
+    let progress_bar = ProgressBar::new({ width * heigth } as u64);
 
-    clone
-        .pixels()
-        .par_bridge()
-        .for_each(|original_pixel| {
-            let (x, y, pixel) = original_pixel;
-            let modified_pixel = palette.closest_to(&pixel);
-            let mut image= image_mutex.lock();
-            image.as_deref_mut().expect("Image Mutex couldnt be dereferenced").put_pixel(x, y, modified_pixel);
-            progress_bar.inc(1);
-        });
+    clone.pixels().par_bridge().for_each(|original_pixel| {
+        let (x, y, pixel) = original_pixel;
+        let modified_pixel = palette.closest_to(&pixel);
+        let mut image = image_mutex.lock();
+        image
+            .as_deref_mut()
+            .expect("Image Mutex couldnt be dereferenced")
+            .put_pixel(x, y, modified_pixel);
+        progress_bar.inc(1);
+    });
     progress_bar.finish();
     let final_image = image_mutex.into_inner();
     match final_image {
         Ok(im) => Ok(im),
-        Err(e) => Err(e)
+        Err(e) => Err(e.into()),
     }
 }
 
-pub fn gruvbox_palette()->Palette{
-    Palette{
-    name: "gruvbox",
-    colors: vec![
-        Rgb([29, 32, 33]),
-        Rgb([40, 40, 40]),
-        Rgb([50, 48, 47]),
-        Rgb([60, 56, 54]),
-        Rgb([80, 73, 69]),
-        Rgb([102, 92, 84]),
-        Rgb([124, 111, 100]),
-        Rgb([124, 111, 100]),
-        Rgb([146, 131, 116]),
-        Rgb([146, 131, 116]),
-        Rgb([249, 245, 215]),
-        Rgb([253, 244, 193]),
-        Rgb([242, 229, 188]),
-        Rgb([235, 219, 178]),
-        Rgb([213, 196, 161]),
-        Rgb([189, 174, 147]),
-        Rgb([168, 153, 132]),
-        Rgb([168, 153, 132]),
-        Rgb([251, 73, 52]),
-        Rgb([184, 187, 38]),
-        Rgb([250, 189, 47]),
-        Rgb([131, 165, 152]),
-        Rgb([211, 134, 155]),
-        Rgb([142, 192, 124]),
-        Rgb([254, 128, 25]),
-        Rgb([204, 36, 29]),
-        Rgb([152, 151, 26]),
-        Rgb([215, 153, 33]),
-        Rgb([69, 133, 136]),
-        Rgb([177, 98, 134]),
-        Rgb([104, 157, 106]),
-        Rgb([214, 93, 14]),
-        Rgb([157, 0, 6]),
-        Rgb([121, 116, 14]),
-        Rgb([181, 118, 20]),
-        Rgb([7, 102, 120]),
-        Rgb([143, 63, 113]),
-        Rgb([66, 123, 88]),
-        Rgb([175, 58, 3]),
-    ]}
+pub fn parse_palette(path: &Path) -> anyhow::Result<Palette> {
+    let mut new_palette = Palette {
+        colors: vec![],
+    };
+
+    let mut file_lines = read_lines(path)?;
+
+    while let Some(possible_line) = file_lines.next() {
+        if possible_line.is_err() {
+            break;
+        }
+
+        let curr_line = possible_line?;
+        new_palette.colors.push(str_to_rgb(curr_line)?);
+    }
+
+    if new_palette.colors.len() == 0 {
+        Err(PaletteErrors::NotEnoughDataError.into())
+    } else {
+        Ok(new_palette)
+    }
+}
+
+fn read_lines<P>(filename: P) -> Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn str_to_rgb(str: String) -> anyhow::Result<Rgb<u8>> {
+    let mut new_rgb = Rgb([0u8, 0u8, 0u8]);
+
+    let iter: Vec<&str> = str.split_ascii_whitespace().collect();
+
+    if iter.len() != 3 {
+        bail!(PaletteErrors::ParseRGBError)
+    }
+
+    for (val, i) in zip(iter, 0..3){
+        new_rgb[i] = val.parse::<u8>()?;
+    }
+
+    Ok(new_rgb)
 }
